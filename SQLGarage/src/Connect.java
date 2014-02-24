0cf7dd68b7f8437fcbbe905e20c7414e564577f0
Cillian Murphy <spacill@murphnduff.org>
2014-02-24 09:47:26 -0800

    Began adding Connect.java code

--- SQLGarage/src/Connect.java
+++ SQLGarage/src/Connect.java
@@ -1,4 +1,33 @@
+import java.sql.*;
 
 public class Connect {
-
+	private Connection con = null;
+	private final String url = "jdbc:sqlserver://";
+	private final String serverName = "fourwaylo.com";
+	private final String portNumber = "8889";
+	private final String databaseName = "csproj";
+	private final String userName = "csproj";
+	private final String password = "DoYourHomework";
+	
+	private final String selectMethod = "cursor";
+	
+	public Connect() {}
+	
+	private String getConnectionURL() {
+		return String.format("%s%s:%s;databaseName=%s;selectMethod=%s;", url, serverName, portNumber, databaseName, selectMethod);
+	}
+	
+	private Connection getConnection() {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerConnection");
+			con = DriverManager.getConnection(getConnectionURL(), userName, password);
+			if(con != null) System.out.println("Connection Successful!");
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Error Trace in getConnection(): " + e.getMessage());
+		}
+		return con;
+	}
+	
+	
 }
