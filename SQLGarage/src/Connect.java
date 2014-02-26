8379328f15e31e0860e01151781754f0db8c5717
Cillian Murphy <spacill@murphnduff.org>
2014-02-26 09:55:26 -0800

    Created VehicleType enum and began creating query generation methods.

--- SQLGarage/src/Connect.java
+++ SQLGarage/src/Connect.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.MissingFormatArgumentException;
 
 public class Connect {
 	private Connection con = null;
@@ -10,6 +11,10 @@ public class Connect {
 	private final String password = "DoYourHomework";
 	
 	private final String selectMethod = "cursor";
+	
+	private static final String INSERT_TEMPLATE = "INSERT INTO %s (%s) VALUES (%s);";
+	private static final String GARAGE_TABLE = "cillian.Garage";
+	private static final String GARAGE_PARAMS = "VehicleType, Make, Model, Year";
 	
 	public Connect() {}
 	
@@ -56,6 +61,29 @@ public class Connect {
 		}
 	}
 	
+	public static String repeatString(String s, int n) {
+		return new String(new char[n]).replace("\0", s);
+	}
+	
+	private boolean addGasCar(String make, String model, int year, int radius, VehicleType type, int... vehicleArgs) throws SQLException {
+		con = this.getConnection();
+		if(con != null) {
+			Statement statement = con.createStatement();
+			String garageQuery = INSERT_TEMPLATE;
+			String garageQueryArgs = String.format("%i, '%s', '%s', %i, %i", type.typeID, make, model, year, radius);
+			garageQuery = String.format(garageQuery, GARAGE_TABLE, GARAGE_PARAMS, garageQueryArgs);
+			statement.addBatch(garageQuery);
+			statement.addBatch("SELECT SCOPE_IDENTITY();");
+			if(statement.executeBatch()[0] >= 1) {
+				ResultSet rs = statement.getResultSet();
+				//statement.addBatch(type.createInsertQuery());
+			} else {
+				throw new IllegalStateException(); //TODO
+			}
+		}
+		return false;
+	}
+	
 	private ResultSet query(String query) throws SQLException {
 		con = this.getConnection();
 		ResultSet rs = null;
@@ -80,9 +108,13 @@ public class Connect {
 	public static void main(String[] args) throws Exception {
 		Connect dbTest = new Connect();
 		//dbTest.displayDbProperties();
-		ResultSet rs = dbTest.query("SELECT * FROM cillian.CGarage INNER JOIN cillian.CGasGarage ON CGarage.ID = CGasGarage.ID;");
+		ResultSet rs = dbTest.query("SELECT * FROM cillian.Garage; SELECT IDENT_CURRENT('cillian.Garage');");
 		if(rs != null) {
-			ResultSetMetaData md = rs.getMetaData();
+			while() {
+				rs.next();
+			}
+			System.out.println(rs.getInt(0));
+			/*ResultSetMetaData md = rs.getMetaData();
 			int columns = md.getColumnCount();
 			for(int i = 1; i <= columns; i++) {
 				System.out.print(md.getColumnName(i) + "\t");
@@ -92,10 +124,38 @@ public class Connect {
 				for(int i = 1; i <= columns; i++)
 					System.out.print(rs.getString(i) + "\t");
 			}
-			md = null;
+			md = null;*/
 			rs.close();
 			rs = null;
 		}
 		dbTest.closeConnection();
+	}
+	
+	private static enum VehicleType {
+		GAS(0, "cillian.GasGarage", "ID, Doors, FuelCapacity, FuelEfficiency, Mileage"),
+		ELECTRIC(1, "cillian.ElectricGarage", "ID, Doors, FuelCapacity, FuelEfficiency, Mileage"),
+		BOAT(2, "cillian.BoatGarage", "ID, Range");
+		
+		VehicleType(int typeID, String table, String params) {
+			this.typeID = typeID;
+			this.params = params;
+			this.table = table;
+		}
+		
+		private final String params;
+		private final String table;
+		private final int typeID;
+		
+		public String createInsertQuery(int ID, int... args) {
+			String query = INSERT_TEMPLATE;
+			String queryArgs = "%i" + repeatString(", %i", args.length);
+			queryArgs = queryArgs.substring(0, queryArgs.length() - 2);
+			System.out.println(queryArgs);
+			try {
+				return String.format(query, table, params, args);
+			} catch(MissingFormatArgumentException e) {
+				throw new IllegalArgumentException(e);
+			}
+		}
 	}
 }
