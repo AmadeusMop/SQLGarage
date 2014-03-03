dab888152a45598b39f2111718d9ebe8e7b7b2a6
Cillian Murphy <spacill@murphnduff.org>
2014-03-03 09:43:17 -0800

    finished auto-formatting for vehicletypes
    added Screen.java

--- SQLGarage/src/Connect.java
+++ SQLGarage/src/Connect.java
@@ -14,7 +14,7 @@ public class Connect {
 	
 	private static final String INSERT_TEMPLATE = "INSERT INTO %s (%s) VALUES (%s);";
 	private static final String GARAGE_TABLE = "cillian.Garage";
-	private static final String GARAGE_PARAMS = "VehicleType, Make, Model, Year";
+	private static final String GARAGE_PARAMS = "VehicleType, Make, Model, Year, SteeringWheelRadius";
 	
 	public Connect() {}
 	
@@ -65,6 +65,37 @@ public class Connect {
 		return new String(new char[n]).replace("\0", s);
 	}
 	
+	private static void printResults(Statement statement) throws SQLException {
+		ResultSet rs;
+		do {
+			rs = statement.getResultSet();
+			if(rs == null) return;
+			printTable(rs);
+			rs.close();
+			System.out.println();
+			System.out.println();
+		} while(statement.getMoreResults());
+		rs.close();
+		rs = null;
+	}
+	
+	public static void printTable(ResultSet rs) throws SQLException {
+		if(rs == null) return;
+		ResultSetMetaData md = rs.getMetaData();
+		int columns = md.getColumnCount();
+		Object[] o = new Object[columns];
+		for(int i = 1; i <= columns; i++) {
+			System.out.print(md.getColumnName(i) + "\t");
+		}
+		while(rs.next()) {
+			System.out.println();
+			for(int i = 1; i <= columns; i++) {
+				System.out.print(rs.getString(i) + "\t");
+			}
+		}
+		md = null;
+	}
+	
 	private boolean addVehicle(String make, String model, int year, int radius, VehicleType type, int... vehicleArgs) throws SQLException {
 		if(!type.verifyArgs(vehicleArgs)) return false;
 		con = this.getConnection();
@@ -72,9 +103,22 @@ public class Connect {
 			Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			String garageQuery = INSERT_TEMPLATE;
 			String garageQueryArgs = String.format("%d, '%s', '%s', %d, %d", type.typeID, make, model, year, radius);
-			String vehicleQuery = type.createInsertQuery("SELECT SCOPE_IDENTITY()", vehicleArgs);
+			String vehicleQuery = type.createInsertQuery("SCOPE_IDENTITY()", vehicleArgs);
 			garageQuery = String.format(garageQuery, GARAGE_TABLE, GARAGE_PARAMS, garageQueryArgs);
 			statement.execute(garageQuery + vehicleQuery);
+			printResults(statement);
+			statement.close();
+		}
+		closeConnection();
+		return true;
+	}
+	
+	private boolean getVehicleInfo(VehicleType type) throws SQLException {
+		con = this.getConnection();
+		if(con != null) {
+			Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			statement.execute(type.createSelectQuery());
+			printResults(statement);
 			statement.close();
 		}
 		closeConnection();
@@ -104,36 +148,7 @@ public class Connect {
 	
 	public static void main(String[] args) throws Exception {
 		Connect dbTest = new Connect();
-		//dbTest.displayDbProperties();
-		dbTest.addVehicle("Toyota", "Notatoyota", 2002, 187, VehicleType.GAS, 7, 140, 53, 6000);
-		Connection con = dbTest.getConnection();
-		Statement statement = con.createStatement();
-		statement.executeQuery("SELECT 0; SELECT * FROM cillian.Garage INNER JOIN cillian.GasGarage ON Garage.ID = GasGarage.ID;");
-		ResultSet rs;
-		while(statement.getMoreResults()) {
-			rs = statement.getResultSet();
-				printTable(rs);
-			rs.close();
-			System.out.println();
-			System.out.println();
-		}
-		rs = null;
 		dbTest.closeConnection();
-	}
-	
-	public static void printTable(ResultSet rs) throws SQLException {
-		ResultSetMetaData md = rs.getMetaData();
-		int columns = md.getColumnCount();
-		for(int i = 1; i <= columns; i++) {
-			System.out.print(md.getColumnName(i) + "\t");
-		}
-		while(rs.next()) {
-			System.out.println();
-			for(int i = 1; i <= columns; i++) {
-				System.out.print(rs.getString(i) + "\t");
-			}
-		}
-		md = null;
 	}
 	
 	private static enum VehicleType {
@@ -153,17 +168,25 @@ public class Connect {
 		private final int typeID;
 		private final int argsLength;
 		
+		public String createSelectQuery() {
+			String query = "SELECT * FROM cillian.Garage INNER JOIN %s ON cillian.Garage.ID = %s.ID;";
+			query = String.format(query, table, table);
+			return query;
+		}
+		
 		public String createInsertQuery(int ID, int... args) {
 			return createInsertQuery(Integer.toString(ID), args);
 		}
 		
 		public String createInsertQuery(String ID, int... args) {
 			String query = INSERT_TEMPLATE;
-			String queryArgs = "%s" + repeatString(", %d", args.length);
-			queryArgs = queryArgs.substring(0, queryArgs.length() - 2);
-			System.out.println(queryArgs);
+			String queryArgs = ID;
+			for(int arg : args) {
+				queryArgs += String.format(", %d", arg);
+			}
+			query = String.format(query, table, params, queryArgs);
 			try {
-				return String.format(query, table, params, args);
+				return query;
 			} catch(MissingFormatArgumentException e) {
 				throw new IllegalArgumentException(e);
 			}
