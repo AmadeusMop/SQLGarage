9f8f089959fc87b4ea57c219035bc7f179c15abf
Cillian Murphy <spacill@murphnduff.org>
2014-03-05 09:55:54 -0800

    Class interrupted, nothing much got done

--- SQLGarage/src/Screen.java
+++ SQLGarage/src/Screen.java
@@ -2,10 +2,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
 
 import javax.swing.*;
 
 
+@SuppressWarnings("serial")
 public class Screen extends JFrame {
 	
 	private JPanel mainPanel;
@@ -35,8 +37,6 @@ public class Screen extends JFrame {
 		//vehicleInfoPanel.setLayout();
         VehicleInfoPanel carPanel = new VehicleInfoPanel.carInfoPanel();
         VehicleInfoPanel boatPanel = new VehicleInfoPanel.boatInfoPanel();
-        setUpVehicleInfoPanel(carPanel);
-        setUpVehicleInfoPanel(boatPanel);
         
         vehicleInfoPanel.addTab("Car", carPanel);
         vehicleInfoPanel.addTab("Boat", boatPanel);
@@ -64,38 +64,68 @@ public class Screen extends JFrame {
 		setVisible(true);
 	}
 	
-	private void setUpVehicleInfoPanel(JPanel panel) {
-		panel.setLayout(new GridLayout(0, 4));
-		panel.add(new JLabel("Make: "));
-		panel.add(new JTextField());
-		panel.add(new JLabel("Model: "));
-		panel.add(new JTextField());
-	}
-	
 	public static void main(String[] args) {
 		Screen s = new Screen();
 	}
 	
 	private abstract static class VehicleInfoPanel extends JPanel {
-		protected GridBagConstraints constraints;
+		
+		/* Field declarations */
+		protected GridBagConstraints C;
 		private JTextField makeField;
 		private JTextField modelField;
 		private JSpinner yearSpinner;
 		private JSpinner radiusSpinner;
 		
+		/* Constructor */
 		public VehicleInfoPanel() {
 			setLayout(new GridBagLayout());
 			
-			constraints = new GridBagConstraints();
+			C = new GridBagConstraints();
+			C.gridx = 0;
+			C.gridy = 0;
+			C.weightx = 0.5;
+			C.weighty = 0.5;
+			
 			makeField = new JTextField();
+			makeField.setPreferredSize(new Dimension(100, 20));
+			
 			modelField = new JTextField();
+			modelField.setPreferredSize(new Dimension(100, 20));
+			
 			yearSpinner = new JSpinner(new SpinnerNumberModel(2015, 1900, 2015, 1));
+			
 			radiusSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 40, 1));
 			
+			C.gridx = 0;
+			add(new JLabel("Make"), C);
+			C.gridx = 1;
+			add(makeField, C);
+
+			C.gridx = 2;
+			add(new JLabel("Model"), C);
+			C.gridx = 3;
+			add(modelField, C);
+
+			C.gridx = 4;
+			add(new JLabel("Year"), C);
+			C.gridx = 5;
+			add(yearSpinner, C);
+			
+			C.gridy = 1;
+			C.gridx = 0;
+			add(new JLabel("Steering Wheel Radius"), C);
+			add(radiusSpinner, C);
+			
+			C.gridy = 2;
 		}
 		
+		/* Abstract method declarations */
 		public abstract int getType();
 		
+		public abstract int[] getSpecificInfo();
+		
+		/* Non-abstract method declarations */
 		public String getMake() {
 			return makeField.getText();
 		}
@@ -112,10 +142,7 @@ public class Screen extends JFrame {
 			return (Integer)radiusSpinner.getValue();
 		}
 		
-		public abstract int[] getSpecificInfo();
-		
-		
-		
+		/* carInfoPanel definition */
 		private static class carInfoPanel extends VehicleInfoPanel implements ActionListener {
 			
 			private ButtonGroup typeGroup;
@@ -138,7 +165,7 @@ public class Screen extends JFrame {
 				type = 0;
 				
 				typeGroup = new ButtonGroup();
-				doorsSpinner = new JSpinner(new SpinnerNumberModel(2015, 1900, 2015, 1));
+				doorsSpinner = new JSpinner(new SpinnerNumberModel(4, 0, 16, 1));
 				fuelCapacitySpinner = new JSpinner(new SpinnerNumberModel(0, 0, 50, 1));
 				fuelEfficiencySpinner = new JSpinner(new SpinnerNumberModel(0, 0, 200, 1));
 				mileageSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 9999999, 1));
@@ -151,28 +178,50 @@ public class Screen extends JFrame {
 				electricButton.addActionListener(this);
 				typeGroup.add(gasButton);
 				typeGroup.add(electricButton);
+				typeGroup.setSelected(gasButton.getModel(), true);
 
-				capacityLabel = new JLabel("c");
-				efficiencyLabel = new JLabel("e");
+				capacityLabel = new JLabel("gallons");
+				efficiencyLabel = new JLabel("mpg");
 				
-				add(new JLabel("Engine Type"));
+				/*add(new JLabel("Engine Type"));
 				add(gasButton);
 				add(electricButton);
+				
+				add(new JLabel("Fuel"));
 				add(fuelCapacitySpinner);
 				add(capacityLabel);
 				add(fuelEfficiencySpinner);
 				add(efficiencyLabel);
+				
+				add(new JLabel("Doors"));
+				add(doorsSpinner);
+				
 				add(new JLabel("Mileage"));
-				add(mileageSpinner);
+				add(mileageSpinner);*/
 			}
 
 			public int[] getSpecificInfo() {
-				// TODO Auto-generated method stub
-				return null;
+				return new int[] { getDoors(), getFuelCapacity(), getFuelEfficiency(), getMileage() };
 			}
 
 			public int getType() {
 				return type;
+			}
+			
+			private int getDoors() {
+				return (Integer)doorsSpinner.getValue();
+			}
+			
+			private int getFuelCapacity() {
+				return (Integer)fuelCapacitySpinner.getValue();
+			}
+			
+			private int getFuelEfficiency() {
+				return (Integer)fuelEfficiencySpinner.getValue();
+			}
+			
+			private int getMileage() {
+				return (Integer)mileageSpinner.getValue();
 			}
 
 			public void actionPerformed(ActionEvent e) {
@@ -182,16 +231,24 @@ public class Screen extends JFrame {
 			}
 		}
 		
-		
+		/* boatInfoPanel definition */
 		private static class boatInfoPanel extends VehicleInfoPanel {
+			private JSpinner rangeSpinner;
 			
 			public boatInfoPanel() {
 				super();
+				
+				rangeSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 9999999, 1));
+				add(new JLabel("Range"));
+				add(rangeSpinner);
 			}
 
 			public int[] getSpecificInfo() {
-				// TODO Auto-generated method stub
-				return null;
+				return new int[] { getRange() };
+			}
+			
+			private int getRange() {
+				return (Integer)rangeSpinner.getValue();
 			}
 
 			public int getType() {
